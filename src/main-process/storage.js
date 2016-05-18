91040abc91f78568d6cca2d5b5a1158cf59bb8c9
ebriand <eric.briand@gmail.com>
2016-05-19 00:45:22 +0200

    begin storage

--- src/main-process/storage.js
+++ src/main-process/storage.js
@@ -1,36 +1,106 @@
-const {app,ipcMain} = require('electron')
+const {app,ipcMain,dialog} = require('electron')
+const storage = require('electron-json-storage');
+const fs = require('fs');
+const path = require('path');
+const templatesPath = app.getPath('userData') + '/templates/';
 
 const defaultImages = [
   {
-    path: app.getAppPath()+'/src/assets/img/defaults/baby.jpg',
-    name: 'Victory Baby'
+    name: 'baby.jpg',
+    title: 'Victory Baby',
+    path: templatesPath + 'baby.jpg'
+  },
+  { 
+    name: 'chapelier.jpg',
+    title: 'Creepy Condescending Wonka',
+    path: templatesPath + 'chapelier.jpg'
   },
   {
-    path: app.getAppPath()+'/src/assets/img/defaults/chapelier.jpg',
-    name: 'Creepy Condescending Wonka'
+    name: 'futurama.jpg',
+    title: 'Futurama Fry',
+    path: templatesPath + 'futurama.jpg'
   },
   {
-    path: app.getAppPath()+'/src/assets/img/defaults/futurama.jpg',
-    name: 'Futurama Fry'
+    name: 'grandma.jpg',
+    title: 'Grandma Finds The Internet',
+    path: templatesPath + 'grandma.jpg'
   },
   {
-    path: app.getAppPath()+'/src/assets/img/defaults/grandma.jpg',
-    name: 'Grandma Finds The Internet'
+    name: 'startrek.png',
+    title: 'Picard Wtf',
+    path: templatesPath + 'startrek.png'
   },
   {
-    path: app.getAppPath()+'/src/assets/img/defaults/startrek.png',
-    name: 'Picard Wtf'
+    name: 'toystory.png',
+    title: 'X, X Everywhere',
+    path: templatesPath + 'toystory.png'
   },
   {
-    path: app.getAppPath()+'/src/assets/img/defaults/toystory.png',
-    name: 'X, X Everywhere'
-  },
-  {
-    path: app.getAppPath()+'/src/assets/img/defaults/taken.jpg',
-    name: 'Liam Neeson Taken'
+    name: 'taken.jpg',
+    title: 'Liam Neeson Taken',
+    path: templatesPath + 'taken.jpg'
   }
 ]
 
-ipcMain.on('get-defaults-images',(e,arg) => {
-  e.sender.send('defaults-images-sended',defaultImages);
+const initTemplatesStorage = () => {
+  fs.mkdir(app.getPath('userData') + '/templates/', () => defaultImages.forEach(copyTemplate));
+  storage.set('templates', defaultImages, (error) => {
+    if (error) throw error;
+  });
+}
+
+const copyTemplate = (template) => {
+  fs.createReadStream(app.getAppPath() + '/src/assets/img/defaults/' + template.name).pipe(fs.createWriteStream(templatesPath + template.name));
+}
+
+const addTemplate = (file,cb) => {
+  const templateName = path.basename(file);
+  fs.createReadStream(file).pipe(fs.createWriteStream(templatesPath + templateName));
+  storage.get('templates', (error, data) => {
+    if (error) throw error;
+    
+    if (data.find(template => template.name == templateName)) {
+      cb();
+      return;
+    }
+    data.push({
+       name: templateName,
+       title: templateName,
+       path: templatesPath + path.basename(file)
+    });
+    storage.set('templates', data, (error) => {
+      if (error) throw error;
+      cb();
+    });
+  });
+}
+
+storage.get('templates', (error, data) => {
+  if (error) throw error;
+
+  if (Object.keys(data).length === 0 && data.constructor === Object) {
+    initTemplatesStorage();
+  }
+});
+
+
+
+ipcMain.on('get-templates',(e,arg) => {
+  storage.get('templates', (error, templates) => {
+    if (error) throw error;
+
+    e.sender.send('templates-sended', templates);
+  });
+  
+})
+
+ipcMain.on('open-file-dialog', (event) => {
+  dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [{name: 'Images', extensions: ['jpg', 'png', 'gif']}]
+  }, (files) => {
+    if (files) {
+      addTemplate(files[0], () => event.sender.send('selected-files'));
+    }
+  })
 })
