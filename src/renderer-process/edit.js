6369e39dfe1a8b6392f5667bbdf960b72fc00c33
FRIN Yvonnick <frin.yvonnick@gmail.com>
2016-05-30 16:27:41 +0200

    Add positionning textareas relative to img

--- src/renderer-process/edit.js
+++ src/renderer-process/edit.js
@@ -1,9 +1,68 @@
 const {ipcRenderer, remote} = require('electron')
+const {setTextareaPosition, limitTextarea} = require('../assets/textareas')
 
+// Tableau contenant les informations relatives aux deux textareas
+const textareas = [
+  {
+    element: document.getElementsByClassName('top')[0],
+    isTop: true,
+    text: 'Insert text here'
+  },
+  {
+    element: document.getElementsByClassName('bottom')[0],
+    isTop: false,
+    text: 'Insert text here'
+  }
+]
+
+// Le conteneur parent de l'image et des textareas
+const editor = document.getElementsByClassName('editor')[0]
+const img = editor.getElementsByTagName('img')[0]
+
+/**
+* Positionne l'ensemble des textareas
+* @return {[type]} [description]
+*/
+const setTextareasPosition = () => {
+  const containerWidth = editor.getBoundingClientRect().width
+  const rect = img.getBoundingClientRect()
+  textareas.map((t) => {
+    setTextareaPosition(t.element, t.isTop, rect, containerWidth)
+  })
+}
+
+// On envoit une demande de récupération d'informations sur le template selectionné
 ipcRenderer.send('get-selected-template', {})
 
+// On receptionne les informations du template selectionné
 ipcRenderer.on('selected-template-sended', (e, i) => {
-  document.getElementsByClassName('editor')[0].getElementsByTagName('img')[0].setAttribute('src', i.path)
+  img.onload = () => {
+    setTextareasPosition(img, textareas, editor)
+    // On initialise le contenu des textareas
+    textareas.map((t, index) => {
+      t.element.getElementsByTagName('textarea')[0].value = t.text
+    })
+  }
+
+  img.setAttribute('src', i.path)
 })
 
-document.getElementById('previous').addEventListener('click', () => remote.getCurrentWindow().close())
+// Au resize de la fenêtre on repositionne les textareas
+window.onresize = () => {
+  setTextareasPosition(img, textareas, editor)
+}
+
+window.onload = () => {
+  // Fix pour focus le premier textarea
+  setTimeout(() => {
+    textareas[0].element.getElementsByTagName('textarea')[0].focus()
+  }, 50)
+}
+
+// On limite le nombre de ligne et de caractères dans les textareas
+textareas.map((t) => {
+  t.element.onkeyup = e => limitTextarea(e.srcElement)
+})
+
+// Action effectuée au click sur le bouton précédent
+document.getElementById('previous').click = () => remote.getCurrentWindow().close()
