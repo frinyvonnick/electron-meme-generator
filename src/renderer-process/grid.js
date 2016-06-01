be752ac84b7079426bfb39d96de6118249d00987
FRIN Yvonnick <frin.yvonnick@gmail.com>
2016-06-01 16:17:04 +0200

    Step-3

--- src/renderer-process/grid.js
+++ src/renderer-process/grid.js
@@ -1,14 +1,40 @@
-const ipc = require('electron').ipcRenderer
 const path = require('path')
 
-ipc.send('get-memes', {})
+const defaultMemes = [
+  {
+    title: 'Victory Baby',
+    file: 'baby.jpg'
+  },
+  {
+    title: 'Creepy Condescending Wonka',
+    file: 'chapelier.jpg'
+  },
+  {
+    title: 'Futurama Fry',
+    file: 'futurama.jpg'
+  },
+  {
+    title: 'Grandma Finds The Internet',
+    file: 'grandma.jpg'
+  },
+  {
+    title: 'Picard Wtf',
+    file: 'startrek.png'
+  },
+  {
+    title: 'X, X Everywhere',
+    file: 'toystory.png'
+  },
+  {
+    title: 'Liam Neeson Taken',
+    file: 'taken.jpg'
+  }
+]
 
-ipc.on('memes-sended', (e, images) => {
-  document.getElementById('content').innerHTML = images.reduce((prev, next, index, arr) => {
-    return `${prev}
-    <div class="card meme" data-index="${index}">
-    <div class="img" style="background-image:url('${path.join('file://', __dirname, '..', 'assets', 'img', 'defaults', next.file)}')"></div>
-    <h3 title="${next.title}"><span>${next.title}</span></h3>
-    </div>`
-  }, '')
-})
+document.getElementById('content').innerHTML = defaultMemes.reduce((prev, next, index, arr) => {
+  return `${prev}
+  <div class="card meme" data-index="${index}">
+  <div class="img" style="background-image:url('${path.join('file://', __dirname, '..', 'assets', 'img', 'defaults', next.file)}')"></div>
+  <h3 title="${next.title}"><span>${next.title}</span></h3>
+  </div>`
+}, '')
