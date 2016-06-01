5a20d34a874bc37701239e78ed5d350586a748e9
FRIN Yvonnick <frin.yvonnick@gmail.com>
2016-06-01 15:29:18 +0200

    Step-1

--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
-app.on('ready', () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 800})
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(path.join('file://', __dirname, 'windows', 'index.html'))
-})
+// Open new window Hello World here!
