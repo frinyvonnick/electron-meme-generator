f97a9db44c50268cdd45ca23aef7eb0d905be5e0
Eric Briand <eric.briand@gmail.com>
2016-11-22 10:11:19 +0100

    fix devtron integration

--- src/assets/client.js
+++ src/assets/client.js
@@ -1,6 +1,10 @@
-if (/--debug/.test(process.argv[4])) {
-  const {remote} = require('electron')
+const {remote} = require('electron')
+
+if (/--debug/.test(remote.process.argv[4])) {
   const client = require('electron-connect').client.create()
+
+  // On install devtron
+  require('devtron').install()
 
   client.on('close', () => {
     remote.getCurrentWindow().close()
